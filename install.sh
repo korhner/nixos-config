0da0301307c4031e69bb15965b8477d2acf2f5ee
Ivan Korhner <korhner@gmail.com>
2023-03-25 00:06:26 +0100

    move swapfile to swap partition as nixos did not recognize it

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 set -e
 
 BOOT_PARTITION_NAME=ESP
+SWAP_PARTITION_NAME=swap
 CRYPTED_PARTITION_NAME=cryptsystem
 DECRYPTED_PARTITION_NAME=system
 
@@ -50,9 +51,11 @@ echo "##########################################################################
 wipefs -af "$disk_name"
 sgdisk -Zo "$disk_name"
 sgdisk --clear \
-       --new=1:0:+550Mib --typecode=1:ef00 --change-name=1:"$BOOT_PARTITION_NAME" \
-       --new=2:0:0       --typecode=1:8309 --change-name=2:"$CRYPTED_PARTITION_NAME" \
-         $disk_name
+       --new=1:0:+550Mib                       --typecode=1:ef00 --change-name=1:"$BOOT_PARTITION_NAME" \
+       --new=2:0:+"$swap_partition_size_mb"Mib --typecode=3:8200 --change-name=3:"$SWAP_PARTITION_NAME" \
+       --new=3:0:0                             --typecode=1:8309 --change-name=2:"$CRYPTED_PARTITION_NAME" \
+         "$disk_name"
+
 wait_seconds=10
 until test $((wait_seconds--)) -eq 0 -o -e /dev/disk/by-partlabel/"$BOOT_PARTITION_NAME" ; do sleep 1; done
 
@@ -60,6 +63,9 @@ echo "##########################################################################
 echo "Formatting partitions"
 echo "#################################################################################################################"
 mkfs.fat -F32 -n $BOOT_PARTITION_NAME /dev/disk/by-partlabel/$BOOT_PARTITION_NAME
+
+mkswap /dev/disk/by-partlabel/$SWAP_PARTITION_NAME
+swapon /dev/disk/by-partlabel/$SWAP_PARTITION_NAME
 
 echo -n "$disk_password" | cryptsetup luksFormat /dev/disk/by-partlabel/$CRYPTED_PARTITION_NAME -d -
 echo -n "$disk_password" | cryptsetup open /dev/disk/by-partlabel/$CRYPTED_PARTITION_NAME $DECRYPTED_PARTITION_NAME -d -
@@ -71,23 +77,14 @@ btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/nix
 btrfs subvolume create /mnt/persist
-btrfs subvolume create /mnt/swap
 
 btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
 umount /mnt
 
-mount -o subvol=root,defaults,compress=lzo,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt
-mount -o subvol=home,defaults,compress=lzo,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt/home
-mount -o subvol=nix,defaults,compress=lzo,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt/nix
-mount -o subvol=persist,defaults,compress=lzo,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt/persist
-
-mount -o subvol=swap,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt/swap
-touch /mnt/swap/swapfile
-chmod 600 /mnt/swap/swapfile
-chattr +C /mnt/swap/swapfile
-dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count="$swap_partition_size_mb"
-mkswap /mnt/swap/swapfile
-swapon /mnt/swap/swapfile
+mount -o subvol=root,compress=zstd,noatime,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt
+mount -o subvol=home,compress=zstd,noatime,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt/home
+mount -o subvol=nix,compress=zstd,noatime,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt/nix
+mount -o subvol=persist,compress=zstd,noatime,X-mount.mkdir LABEL="$DECRYPTED_PARTITION_NAME" /mnt/persist
 
 mount -o defaults,X-mount.mkdir LABEL="$BOOT_PARTITION_NAME" /mnt/boot
 
@@ -96,3 +93,6 @@ echo "Setup base system"
 echo "#################################################################################################################"
 nixos-generate-config --root /mnt
 
+# TODO issues:
+# - swapDevices not populated in hardware configuration
+# - btrfs options not populated in hardware configuration (noatime, compression)
