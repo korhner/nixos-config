6448aff8ebe2bb1338048532eb361f9541578034
Ivan Korhner <korhner@gmail.com>
2023-03-24 22:53:58 +0100

    add swap

--- install.sh
+++ install.sh
@@ -71,6 +71,7 @@ btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/nix
 btrfs subvolume create /mnt/persist
+btrfs subvolume create /mnt/swap
 
 btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
 umount /mnt
