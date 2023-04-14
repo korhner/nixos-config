7499e7dae3350d2149a17297810fefebd3359700
Ivan Korhner <korhner@gmail.com>
2023-04-15 00:52:24 +0200

    various refactors

---
+++ hosts/workstation-dell-vostro-15-5510/disko.nix
@@ -0,0 +1,64 @@
+{ ... }: {
+
+disko.devices = {
+  disk = {
+    sda = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            type = "partition";
+            name = "ESP";
+            start = "1MiB";
+            end = "550MiB";
+            fs-type = "fat32";
+            bootable = true;
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          }
+          {
+            type = "partition";
+            name = "cryptsystem";
+            start = "550MiB";
+            end = "-4G";
+            content = {
+              type = "luks";
+              name = "system";
+              content = {
+                type = "btrfs";
+                mountOptions = [ "compress=zstd" "noatime" ];
+                extraArgs = [ "--label system" ];
+                subvolumes = {
+                  "/root" = { mountpoint = "/"; };
+                  "/nix" = {};
+                  "/home" = {};
+                  "/persist" = {};
+                };
+              };
+            };
+          }
+          {
+            name = "swap";
+            type = "partition";
+            start = "-4G";
+            end = "100%";
+            part-type = "primary";
+            content = {
+              type = "swap";
+              randomEncryption = true;
+            };
+          }
+        ];
+      };
+    };
+  };
+  };
+
+  fileSystems."/persist".neededForBoot = true;
+}
--- script/format.sh
+++ script/format.sh
@@ -5,6 +5,6 @@ HOST="workstation-dell-vostro-15-5510"
 mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
 
-nix run github:nix-community/disko -- -m zap_create_mount ../hosts/$HOST/hardware-configuration.nix
+nix run github:nix-community/disko -- -m zap_create_mount ../hosts/$HOST/disko.nix
 
 nixos-generate-config --root /mnt
