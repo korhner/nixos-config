d9e63aeeea4e802ecd0dc4ea1589744a1bda2010
Ivan Korhner <korhner@gmail.com>
2023-04-14 18:35:09 +0200

    some fixes

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -2,8 +2,8 @@
 { lib, inputs, outputs, ... }:
 {
   imports = [
-#    inputs.impermanence.nixosModules.impermanence
-#    inputs.home-manager.nixosModules.home-manager
+    inputs.impermanence.nixosModules.impermanence
+    inputs.home-manager.nixosModules.home-manager
     ./docker.nix
     ./locale.nix
     ./nix.nix
@@ -21,16 +21,17 @@
     };
   };
 
-#  environment = {
-#    persistence = {
-#      "/persist".directories = [
-#        "/var/lib/systemd"
-#        "/var/log"
-#        "/srv"
-#      ];
-#    };
-#    enableAllTerminfo = true;
-#  };
+  environment = {
+    hideMounts = true;
+    persistence = {
+      "/persist".directories = [
+        "/var/lib/systemd"
+        "/var/log"
+        "/srv"
+      ];
+    };
+    enableAllTerminfo = true;
+  };
 
   programs.fuse.userAllowOther = true;
   hardware.enableRedistributableFirmware = true;
