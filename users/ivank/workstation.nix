b0be7d3b38ebb43ed163c174443db65ddbd681c0
Ivan Korhner <korhner@gmail.com>
2023-04-14 22:45:59 +0200

    close to make it work

--- users/ivank/workstation.nix
+++ users/ivank/workstation.nix
@@ -15,37 +15,37 @@
       # TODO add others
     ];
 
-  home-manager.users.ivank = {
-    #  ------   -----   ------
-    # | DP-3 | | DP-1| | DP-2 |
-    #  ------   -----   ------
-    #  monitors = [
-    #    {
-    #      name = "DP-3";
-    #      width = 1920;
-    #      height = 1080;
-    #      noBar = true;
-    #      x = 0;
-    #      workspace = "3";
-    #      enabled = false;
-    #    }
-    #    {
-    #      name = "DP-1";
-    #      width = 2560;
-    #      height = 1080;
-    #      refreshRate = 75;
-    #      x = 1920;
-    #      workspace = "1";
-    #    }
-    #    {
-    #      name = "DP-2";
-    #      width = 1920;
-    #      height = 1080;
-    #      noBar = true;
-    #      x = 4480;
-    #      workspace = "2";
-    #    }
-    #  ]
-  };
+#  home-manager.users.ivank = {
+#    #  ------   -----   ------
+#    # | DP-3 | | DP-1| | DP-2 |
+#    #  ------   -----   ------
+#    #  monitors = [
+#    #    {
+#    #      name = "DP-3";
+#    #      width = 1920;
+#    #      height = 1080;
+#    #      noBar = true;
+#    #      x = 0;
+#    #      workspace = "3";
+#    #      enabled = false;
+#    #    }
+#    #    {
+#    #      name = "DP-1";
+#    #      width = 2560;
+#    #      height = 1080;
+#    #      refreshRate = 75;
+#    #      x = 1920;
+#    #      workspace = "1";
+#    #    }
+#    #    {
+#    #      name = "DP-2";
+#    #      width = 1920;
+#    #      height = 1080;
+#    #      noBar = true;
+#    #      x = 4480;
+#    #      workspace = "2";
+#    #    }
+#    #  ]
+#  };
 
 }
