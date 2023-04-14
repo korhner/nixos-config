81b333bc2cdb49b14dbfa442f6483e1e8cba0c3a
Ivan Korhner <korhner@gmail.com>
2023-04-14 15:20:52 +0200

    some fixes

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -2,7 +2,7 @@
 { lib, inputs, outputs, ... }:
 {
   imports = [
-#    inputs.impermanence.nixosModules.impermanence
+    inputs.impermanence.nixosModules.impermanence
     inputs.home-manager.nixosModules.home-manager
     ./docker.nix
     ./locale.nix
@@ -23,7 +23,11 @@
 
   environment = {
     persistence = {
-      "/persist".directories = [ "/var/lib/systemd" "/var/log" "/srv" ];
+      "/persist".directories = [
+        "/var/lib/systemd"
+        "/var/log"
+        "/srv"
+      ];
     };
     enableAllTerminfo = true;
   };
