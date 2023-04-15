6340610e4c8c441005b06cb53d62a41482d31272
Ivan Korhner <korhner@gmail.com>
2023-04-15 22:10:53 +0200

    various refactors

--- hosts/workstation-dell-vostro-15-5510/hardware-configuration.nix
+++ hosts/workstation-dell-vostro-15-5510/hardware-configuration.nix
@@ -8,7 +8,7 @@
   boot = {
     kernelParams = [ "boot.shell_on_fail" ];
     initrd = {
-      availableKernelModules = [ "ata_piix" "ahci" "sr_mod" "mptspi" "virtio_blk" ];
+      availableKernelModules = [ "ata_piix" "ahci" "sr_mod" "mptspi" ];
       kernelModules = [ ];
     };
     loader = {
