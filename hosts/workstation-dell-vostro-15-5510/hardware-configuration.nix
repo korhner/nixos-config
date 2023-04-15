0bc2d629b52cf878cf29835ab8390c939de9e780
Ivan Korhner <korhner@gmail.com>
2023-04-15 19:13:05 +0200

    various refactors

--- hosts/workstation-dell-vostro-15-5510/hardware-configuration.nix
+++ hosts/workstation-dell-vostro-15-5510/hardware-configuration.nix
@@ -5,17 +5,24 @@
 #    ../common/optional/emphemeral-btrfs.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.loader.grub = {
-    enable = true;
-    version = 2;
-    efiSupport = true;
-    enableCryptodisk = true;
-    device = "nodev";
+  boot = {
+    kernelParams = [ "boot.shell_on_fail" ];
+    initrd = {
+      availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
+      kernelModules = [ ];
+    };
+    loader = {
+      systemd-boot.enable = true;
+      grub = {
+        enable = true;
+        version = 2;
+        efiSupport = true;
+        enableCryptodisk = true;
+        device = "nodev";
+       };
+      efi.canTouchEfiVariables = true;
+    };
   };
-
 
   nixpkgs.hostPlatform.system = "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = true;
