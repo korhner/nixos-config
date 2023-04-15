c66984f49592eceb6b54412d43bab693a8a8418b
Ivan Korhner <korhner@gmail.com>
2023-04-15 12:48:28 +0200

    various refactors

--- script/format.sh
+++ script/format.sh
@@ -6,3 +6,6 @@ mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
 
 nix run github:nix-community/disko -- -m zap_create_mount ../hosts/$HOST/disko.nix
+
+# Generate config so it can be inspected
+nixos-generate-config --root /home/root
