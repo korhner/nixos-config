82abd43970ba6a1ae2b16c889cc0e7c0045e4d9b
Ivan Korhner <korhner@gmail.com>
2023-04-16 22:37:39 +0200

    experiment with impermanence

--- hosts/common/optional/emphemeral-btrfs.nix
+++ hosts/common/optional/emphemeral-btrfs.nix
@@ -6,7 +6,7 @@
     mkdir /tmp -p
     MNTPOINT=$(mktemp -d)
     (
-      mount -t btrfs -o subvol=/ /dev/disk/by-label/${hostname} "$MNTPOINT"
+      mount -t btrfs -o subvol=/ /dev/mapper/crypted "$MNTPOINT"
       trap 'umount "$MNTPOINT"' EXIT
       echo "Cleaning root subvolume"
       btrfs subvolume list -o "$MNTPOINT/root" | cut -f9 -d ' ' |
