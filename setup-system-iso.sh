66e456dc50fbc324a8c70ba3c4f724c3aa9154b4
Ivan Korhner <korhner@gmail.com>
2023-04-18 20:12:52 +0200

    experiment with impermanence

--- setup-system-iso.sh
+++ setup-system-iso.sh
@@ -22,6 +22,7 @@ echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
 nixos-rebuild build --flake .#"${HOST}"
 ./result/sw/bin/setup-system-iso
 
-mv ../nixos-config /persist/home/$USER/repositories
+mkdir /mnt/persist/home/$USER/repositories
+mv ../nixos-config /mnt/persist/home/$USER/repositories
 
 reboot
