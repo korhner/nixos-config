c1e297238ecb9186aec834e1354864e6e3ea12ff
Ivan Korhner <korhner@gmail.com>
2023-04-21 22:34:08 +0200

    merge home manager into system config

--- hosts/work-dell/hardware-configuration.nix
+++ hosts/work-dell/hardware-configuration.nix
@@ -3,18 +3,18 @@
     ./disko.nix
   ];
 
-#  boot = {
-#    kernelParams = [ "boot.shell_on_fail" ];
-#    initrd = {
-#      availableKernelModules = [ "ata_piix" "ahci" "sr_mod" "mptspi" ];
-#      kernelModules = [ ];
-#    };
-#    loader = {
-#      systemd-boot.enable = true;
-#      efi.canTouchEfiVariables = true;
-#    };
-#  };
+  boot = {
+    kernelParams = [ "boot.shell_on_fail" ];
+    initrd = {
+      availableKernelModules = [ "ata_piix" "ahci" "sr_mod" "mptspi" ];
+      kernelModules = [ ];
+    };
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
-#  nixpkgs.hostPlatform.system = "x86_64-linux";
+  nixpkgs.hostPlatform.system = "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = true;
 }
