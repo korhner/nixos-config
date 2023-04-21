3b4848198efa3de61b32bc448b942399b8499a1a
Ivan Korhner <korhner@gmail.com>
2023-04-21 22:14:57 +0200

    merge home manager into system config

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.ivank = import ./users/ivank/workstation.nix;
+#            home-manager.users.ivank = import ./users/ivank/workstation.nix;
           }
         ];
       };
