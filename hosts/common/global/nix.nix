3111bf71c9c4d6396baad44ecad25e19b6bf2bb9
Ivan Korhner <korhner@gmail.com>
2023-04-22 11:49:13 +0200

    merge home manager into system config

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -1,13 +1,13 @@
 { pkgs, inputs, lib, config, ... }:
 {
   nix = {
-#    settings = {
-#      trusted-users = [ "root" "@wheel" ];
-#      auto-optimise-store = lib.mkDefault true;
-#      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
-#      warn-dirty = false;
-#      system-features = [ "kvm" "big-parallel" ];
-#    };
+    settings = {
+      trusted-users = [ "root" "@wheel" ];
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      warn-dirty = false;
+      system-features = [ "kvm" "big-parallel" ];
+    };
 #    package = pkgs.nixFlakes;
 #    gc = {
 #      automatic = true;
