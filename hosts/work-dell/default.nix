19f6e10395f83bdbf3f15173e0e28caf5ad31a75
Ivan Korhner <korhner@gmail.com>
2023-04-22 11:31:48 +0200

    merge home manager into system config

--- hosts/work-dell/default.nix
+++ hosts/work-dell/default.nix
@@ -13,28 +13,28 @@
 #      common-gpu-nvidia-disable = import ./common/gpu/nvidia/disable.nix;
 #      common-hidpi = import ./common/hidpi.nix;
     ./hardware-configuration.nix
-#    ../common/global
-#    ../common/optional/emphemeral-btrfs.nix
-#    ../common/optional/xfce.nix
+    ../common/global
+    ../common/optional/emphemeral-btrfs.nix
+    ../common/optional/xfce.nix
   ];
 
-#  programs.fish.enable = true;
-#  users.users = {
-#    ivank = {
-#      initialPassword = "qwe123";
-#      isNormalUser = true;
-#      extraGroups = [ "wheel" ];
-#
-#      shell = pkgs.fish;
-#    };
-#  };
-#
-#  programs.fuse.userAllowOther = true;
-#
-#  systemd.tmpfiles.rules = [
-#      "d /home/ivank 0700 ivank -"
-#      "d /persist/home/ivank 0700 ivank -"
-#  ];
+  programs.fish.enable = true;
+  users.users = {
+    ivank = {
+      initialPassword = "qwe123";
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+
+      shell = pkgs.fish;
+    };
+  };
+
+  programs.fuse.userAllowOther = true;
+
+  systemd.tmpfiles.rules = [
+      "d /home/ivank 0700 ivank -"
+      "d /persist/home/ivank 0700 ivank -"
+  ];
 
   networking.hostName = "work-dell";
   system.stateVersion = "22.11";
