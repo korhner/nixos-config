1a0cf05dd90174a16ac41ac6deacd9a7b2213d9c
Ivan Korhner <korhner@gmail.com>
2023-04-22 11:39:17 +0200

    merge home manager into system config

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     };
   };
 
-  outputs = { nixpkgs, disko, inputs, ... }: {
+  outputs = inputs@{ nixpkgs, disko,  ... }: {
     nixosConfigurations = {
       "work-dell" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
