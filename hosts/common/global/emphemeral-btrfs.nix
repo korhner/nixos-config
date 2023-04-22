751b564593225313f85030e9fe93c4c79f9a34e8
Ivan Korhner <korhner@gmail.com>
2023-04-22 14:25:35 +0200

    refactoring, try again

--- hosts/common/global/emphemeral-btrfs.nix
+++ hosts/common/global/emphemeral-btrfs.nix
@@ -1,56 +1,59 @@
 # https://grahamc.com/blog/erase-your-darlings/
 # https://mt-caret.github.io/blog/posts/2020-06-29-optin-state.html
 
-{ pkgs, lib, config, inputs, ... }: {
+{ pkgs, lib, config, inputs, ... }:
+let
+  hostname = config.networking.hostName;
+
+  setupSystemIso = pkgs.writeShellScriptBin "setup-system-iso" ''
+    if [ "$(hostname)" != "nixos" ]
+    then
+      echo "Must be run on live image"
+      exit 1
+    fi
+
+    ${config.system.build.formatScript}
+    ${config.system.build.mountScript}
+
+    nixos-install --flake .#"${hostname}" --no-root-passwd
+  '';
+
+  impermanenceDiff = pkgs.writeShellScriptBin "impermanence-diff" ''
+    mkdir /tmp -p
+    MNTPOINT=$(mktemp -d)
+    (
+      mount -t btrfs -o subvol=/ /dev/mapper/crypted "$MNTPOINT"
+      trap 'umount "$MNTPOINT"' EXIT
+
+      btrfs subvolume snapshot -r /$MNTPOINT/root /$MNTPOINT/root-current
+      btrfs send -p /$MNTPOINT/root-blank /$MNTPOINT/root-current --no-data | btrfs receive --dump
+      btrfs subvolume snapshot delete /$MNTPOINT/root-current
+
+      btrfs subvolume snapshot -r /$MNTPOINT/home /$MNTPOINT/home-current
+      btrfs send -p /$MNTPOINT/home-blank /$MNTPOINT/home-current --no-data | btrfs receive --dump
+      btrfs subvolume snapshot delete /$MNTPOINT/home-current
+    )
+  '';
+
+in {
 
   imports = [
-    inputs.impermanence.nixosModules.impermanence
+#    inputs.impermanence.nixosModules.impermanence
   ];
 
   fileSystems."/persist".neededForBoot = true;
 
-  environment.persistence."/persist" = {
-    directories = [
-      "/var/lib/systemd"
-      "/var/log"
-    ];
-    files = [
-      "/etc/machine-id"
-    ];
-  };
+#  environment.persistence."/persist" = {
+#    directories = [
+#      "/var/lib/systemd"
+#      "/var/log"
+#    ];
+#    files = [
+#      "/etc/machine-id"
+#    ];
+#  };
 
-  environment.systemPackages = [
-    pkgs.writeShellScriptBin "setup-system-iso" ''
-      if [ "$(hostname)" != "nixos" ]
-      then
-        echo "Must be run on live image"
-        exit 1
-      fi
-
-      ${config.system.build.formatScript}
-      ${config.system.build.mountScript}
-
-      nixos-install --flake .#work-dell --no-root-passwd
-    ''
-
-    pkgs.writeShellScriptBin "impermanence-diff" ''
-      mkdir /tmp -p
-      MNTPOINT=$(mktemp -d)
-      (
-        mount -t btrfs -o subvol=/ /dev/mapper/crypted "$MNTPOINT"
-        trap 'umount "$MNTPOINT"' EXIT
-
-        btrfs subvolume snapshot -r /$MNTPOINT/root /$MNTPOINT/root-current
-        btrfs send -p /$MNTPOINT/root-blank /$MNTPOINT/root-current --no-data | btrfs receive --dump
-        btrfs subvolume snapshot delete /$MNTPOINT/root-current
-
-        btrfs subvolume snapshot -r /$MNTPOINT/home /$MNTPOINT/home-current
-        btrfs send -p /$MNTPOINT/home-blank /$MNTPOINT/home-current --no-data | btrfs receive --dump
-        btrfs subvolume snapshot delete /$MNTPOINT/home-current
-      )
-    ''
-
-  ];
+  environment.systemPackages = [ setupSystemIso impermanenceDiff ];
 
   boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
     mkdir /tmp -p
