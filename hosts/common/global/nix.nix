2fe5df71c602a082863a1ae3bb2643bb9d2759ed
Ivan Korhner <korhner@gmail.com>
2023-04-22 11:51:11 +0200

    merge home manager into system config

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -8,18 +8,18 @@
       warn-dirty = false;
       system-features = [ "kvm" "big-parallel" ];
     };
-#    package = pkgs.nixFlakes;
-#    gc = {
-#      automatic = true;
-#      dates = "weekly";
-#      # Delete older generations too
-#      options = "--delete-older-than 7d";
-#    };
-#
-#    # Add each flake input as a registry
-#    # To make nix3 commands consistent with the flake
-#    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-#
+    package = pkgs.nixFlakes;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Delete older generations too
+      options = "--delete-older-than 7d";
+    };
+
+    # Add each flake input as a registry
+    # To make nix3 commands consistent with the flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
 #    # Map registries to channels
 #    # Very useful when using legacy commands
 #    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
