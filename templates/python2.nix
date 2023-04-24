cdcbe7e7bdec3bfdc0f53b2ca15aea74aa243131
Ivan Korhner <korhner@gmail.com>
2023-04-24 23:08:23 +0200

    direnv

---
+++ templates/python2.nix
@@ -0,0 +1,23 @@
+{ pkgs }:
+let
+  python = pkgs.python310;
+in
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs }: {
+    defaultPackage.x86_64-linux = python.withPackages (ps: with ps; [
+      pkgs.pip
+    ]);
+    devShell.x86_64-linux = python.mkShell {
+      buildInputs = with pkgs; [
+        pkgs.pip
+      ];
+    };
+    shellHook = ''
+      export PYTHONPATH="${PYTHONPATH:+$PYTHONPATH:}$PWD"
+    '';
+    packages = import ./requirements.txt { inherit pkgs; python = python; };
+  };
+}
